ee0eabff095e2b4ad96e6fe9569db138586e9379
marcio-rdg <o.nero.psd@gmail.com>
2026-02-20 15:45:52 -0300

    feat: :art: section calculator

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,38 @@
 import '@/scss/style.scss';
 
-function auditSite() {
-  const urlInput = document.getElementById('siteUrl');
-  const url = urlInput.value.trim();
+document.addEventListener('DOMContentLoaded', () => {
+  function auditSite() {
+    const urlInput = document.getElementById('siteUrl');
+    const url = urlInput.value.trim();
 
-  if (!url) {
-    // Um feedback visual rápido é melhor que um alert,
-    // mas para o MVP, o alert resolve.
-    alert('Por favor, insira a URL do seu site.');
-    return;
+    if (!url) {
+      return;
+    }
+
+    const message = `Olá Márcio! Acabei de solicitar uma auditoria express para o meu site: ${url}`;
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappNumber = '5533999486162';
+    window.open(
+      `https://wa.me/${whatsappNumber}?text=${encodedMessage}`,
+      '_blank'
+    );
   }
 
-  // Higienização básica da string para o link do WhatsApp
-  const message = `Olá Márcio! Acabei de solicitar uma auditoria express para o meu site: ${url}`;
-  const encodedMessage = encodeURIComponent(message);
-  const whatsappNumber = '5533000000000'; // Seu número real aqui
+  const adutiBtn = document.querySelector('#auditButton');
+  adutiBtn.addEventListener('click', auditSite);
 
-  window.open(
-    `https://wa.me/${whatsappNumber}?text=${encodedMessage}`,
-    '_blank'
-  );
-}
+  const ranges = document.querySelectorAll('input[type="range"]');
+
+  ranges.forEach((range) => {
+    const updateProgress = () => {
+      const min = range.min ? parseFloat(range.min) : 0;
+      const max = range.max ? parseFloat(range.max) : 100;
+      const val = parseFloat(range.value);
+      const percentage = ((val - min) / (max - min)) * 100;
+      range.style.setProperty('--range-progress', `${percentage}%`);
+    };
+
+    range.addEventListener('input', updateProgress);
+    updateProgress();
+  });
+});
